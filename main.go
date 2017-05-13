b424ecd79ba44f70a11ab6a59e68be2b9f035e52
bamchoh <bamchoh@gmail.com>
2017-05-13 14:32:32 +0900

    Add mstdn-polly source code

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"github.com/bamchoh/pollydent"
+)
+
+type message struct {
+	ID       uint
+	UserName string `json:"username"`
+	Acct     string `json:"acct"`
+	Content  string `json:"content"`
+}
+
+func setLog(prefix string) *log.Logger {
+	basedir := filepath.Dir(os.Args[0])
+	logPath := filepath.Join(basedir, prefix+".log")
+	f, _ := os.Create(logPath)
+	return log.New(f, prefix+":", 0)
+}
+
+func main() {
+	logger := setLog("mstdn")
+
+	p, err := pollydent.NewPolly(logger, "mstdn-polly.yml")
+	if err != nil {
+		logger.Println(err)
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		dec := json.NewDecoder(strings.NewReader(txt))
+		msg := new(message)
+		err := dec.Decode(msg)
+		if err != nil {
+			logger.Println(err)
+		}
+		reg := regexp.MustCompile("https?://.*")
+		readContent := reg.ReplaceAllString(msg.Content, "")
+		go p.ReadAloud(readContent)
+	}
+
+	if err := scanner.Err(); err != nil {
+		logger.Println("reading standard input:", err)
+	}
+}
